72f61619ab660289068d0327ce226581dbb6c90d
thinkoco <thinkoco@foxmail.com>
2018-02-08 15:07:28 +0800

    update de10_nano binaris

---
+++ compiled_bin_sdk16.1/de10_nano/init_opencl.sh
@@ -0,0 +1,5 @@
+export ALTERAOCLSDKROOT=/home/root/opencl_arm32_rte
+export AOCL_BOARD_PACKAGE_ROOT=$ALTERAOCLSDKROOT/board/c5soc
+export PATH=$ALTERAOCLSDKROOT/bin:$PATH
+export LD_LIBRARY_PATH=$ALTERAOCLSDKROOT/host/arm32/lib:$LD_LIBRARY_PATH
+insmod $AOCL_BOARD_PACKAGE_ROOT/driver/aclsoc_drv.ko
